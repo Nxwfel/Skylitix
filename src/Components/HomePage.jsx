a80c247525c0522b121159f3990115c122a10d87
Nxwfel <nelxpe@gmail.com>
2025-03-28 04:09:44 +0000

    Responsivness bugs fixed

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -9,7 +9,9 @@ gsap.registerPlugin(ScrollTrigger)
 
 
 const HomePage = () => {
+  let mm = gsap.matchMedia()
     useEffect(() => {
+       mm.add("(min-width:1024px)", () =>{ 
         gsap.to(".Earth", {
           scrollTrigger: {
             trigger: ".Earth",
@@ -20,16 +22,16 @@ const HomePage = () => {
             markers: true,
           },
           scale: 0.5,
-          y: 50,
+          y: 70,
           x: 350,
           rotation: 100,
           delay: 1,
-        });
+        })});
       }, []); 
   return (
     <div className='h-fit w-screen flex flex-col justify-center items-center bg-black overflow-hidden '>
      <Header />
-     <img src={Earth} alt="Earth"  className='min-md:Earth h-[190vh] max-md:h-[85vh] max-sm:h-[70vh] w-screen mt-80 z-0'/>
+     <img src={Earth} alt="Earth"  className='Earth h-[190vh] max-lg:h-[110vh] max-md:h-[85vh] max-sm:h-[70vh] w-screen mt-80 z-0'/>
      <div className='absolute h-70 w-100 top-60 z-10 text-center items-center flex flex-col'>
         <p className='font-Alumi  text-xl text-white font-light'>Start-up that helps the farmers</p>
         <h1 className='font-Alumi text-7xl text-white font-bold tracking-widest'>Skylitix: <br /> <span className='font-normal tracking-wide text-nowrap max-md:text-5xl'>Satellites In Agronomia</span> </h1>
@@ -57,13 +59,13 @@ const HomePage = () => {
      initial={{ opacity:0 ,x:-100 }}
      whileInView={{ opacity:1, x:0}}
      transition={{ duration: 2 }}
-     className='absolute max-md:relative h-screen w-[60vw] top-[140vh] max-md:top-30 max-sm:top-50 max-md:text-center max-md:items-center max-md:text-nowrap max-md:w-screen left-4 max-md:left-0 z-10 text-start flex flex-col'>
+     className='absolute max-md:relative h-screen w-[60vw] top-[140vh] max-md:top-30 max-sm:top-50 max-md:text-center max-md:items-center max-md:text-nowrap max-md:w-screen left-4 max-md:left-0 z-20 text-start flex flex-col '>
       <h1 className='text-9xl text-white font-Alumi font-bold'>Skylitix</h1>
       <p className='text-xl text-wrap max-w-[40vw] max-md:max-w-full  text-white font-Alumi font-light'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam rem minus ex eius, nemo voluptate sit quibusdam illum excepturi cumque neque, praesentium reprehenderit eos quam sint adipisci ratione consectetur. Tempora.
       </p>
      </motion.div>
-     <div className='h-screen w-screen -mt-40 max-md:-mt-20 max-sm:mt-20 mb-5 text-center items-center justify-center flex flex-col'>
+     <div className='h-screen w-screen -mt-40 max-lg:mt-100 max-md:-mt-20 max-sm:mt-20 mb-5 text-center items-center justify-center flex flex-col'>
      
       <motion.h1 
       initial={{opacity:-10, y:100}}
